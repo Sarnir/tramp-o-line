69c86fe61a64db68b3c4d79cf6d32867ff0fb421
Sarnir <so.called.piter@gmail.com>
2015-01-18 17:51:31 +0100

    input handler

--- tramp-o-line/Assets/TrampController.cs
+++ tramp-o-line/Assets/TrampController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class TrampController : MonoBehaviour {
 
+    public int horizontalSpeed;
+
+    Trick currentTrick { get; set; }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,14 +14,21 @@ public class TrampController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        var currentSpeed = rigidbody2D.velocity;
+
+        if (Input.GetKeyDown(KeyCode.C))
+            print("C pressed");
+
+        if (Input.GetKey(KeyCode.RightArrow) && currentSpeed.x < horizontalSpeed * 4)
         {
             print("right arrow");
+            rigidbody2D.AddForce(new Vector2(horizontalSpeed, 0));
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && currentSpeed.x > -horizontalSpeed * 4)
         {
             print("left arrow");
+            rigidbody2D.AddForce(new Vector2(-horizontalSpeed, 0));
         }
 	}
 }
