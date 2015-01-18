c25ad272bc09a1f8bf2a6dcbcfe7d9c94cf0894e
Edvardius <mtyszkiewicz@gmail.com>
2015-01-18 19:59:13 +0100

    dziwoty

---
+++ tramp-o-line/Assets/BlockRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockRotation : MonoBehaviour {
+
+	public GameObject tramp;
+
+	void Start () {
+	
+	}
+
+	void Update ()
+	{
+		gameObject.transform.position =
+			new Vector3(tramp.transform.position.x, tramp.transform.position.y,
+			            gameObject.transform.position.z);
+	}
+}
